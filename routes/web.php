5433ada41cac6a46787270406cae5a93df3bb7dc
renzo <rfens18@hotmail.com>
2020-12-11 10:38:33 +0100

    login toegevoegd

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('/cars', function () {
     return view('public.cars');
 });
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
